f32a6ca75c26a8b0d3621a9f48c58971c8524e44
Sam-spike kemp <Sam-spike kemp>
2017-03-05 21:09:39 +0000

    Doing writing to files

--- api/game/gameGame.js
+++ api/game/gameGame.js
@@ -2,20 +2,24 @@
 
 var path = require('path');
 let fs = require('fs');
-var readStuff ='';
-fs.readFile(path.join(__dirname+'/gameJSON.json'),'utf-8',(err, data) => {
+var readStuff = '';
+fs.readFile(path.join(__dirname + '/gameJSON.json'), 'utf-8', (err, data) => {
     if (err) {
         console.log(err);
     }
     readStuff = data;
     var parsedDATA = JSON.parse(readStuff); // parsing JSON to utilise as an object
     console.log(parsedDATA); // logging to show test data
-    parsedDATA.name = 'newParsedStuff'; // utilise the functionality of an object to make sure its been parsed
-    readStuff = parsedDATA; 
+    parsedDATA.name = 'parsedStuff'; // utilise the functionality of an object to make sure its been parsed
+    readStuff = parsedDATA;
     module.exports.a = parsedDATA; // update the details in module.exports
     console.log(readStuff);
+    console.log(JSON.stringify(readStuff));
+    fs.writeFile(path.join(__dirname + '/gameJSON.json'), JSON.stringify(readStuff), 'utf-8', (err) => {
+        if (err) throw err;
+        console.log('It\'s saved!');
+    });
 });
 module.exports = {
     a: readStuff // this stays as an empty string unless its updated
 };
-console.log(readStuff);
