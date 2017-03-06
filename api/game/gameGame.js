22e69c52f51fa51ca291612ae87059c8d59ebadd
Sam-spike kemp <Sam-spike kemp>
2017-03-06 08:48:15 +0000

    Added comments to the index + gameGame js files

--- api/game/gameGame.js
+++ api/game/gameGame.js
@@ -33,16 +33,16 @@ let baseFile = {
 module.exports = {
     //a: readStuff, // this stays as an empty string unless its updated
     //callback will be called with (err, fileData)
-    getSettings: (callback) => {
-        fs.readFile(file, (err, data) => {
+    getSettings: (callback) => { // getSettings is the key, it holds a function with a callback as its argument
+        fs.readFile(file, (err, data) => { //fs.readFile the file at that path (file), when that finishes no matter how it goes go to the next function
             if (err) {
                 //file not there, we must create it from the fires of hell
                 console.log('save file doesn\'t exist, creating');
-                fs.writeFile(file, JSON.stringify(baseFile), (err) => {
-                    callback(err, baseFile);
+                fs.writeFile(file, JSON.stringify(baseFile), (err) => { // as there's no file we write a file instead of read, we right the JSON of the basefile 
+                    callback(err, baseFile); // end the get getSettings function with this callback, returning err(if there is one, hopefully this'll be null/undef) and the js object baseFile
                 });
             } else {
-                callback(null, JSON.parse(data));
+                callback(null, JSON.parse(data));// additional end to function, there was no error so instead of writing a file we read and parse the file that we need, we would then apply the setting
             }
         });
     }
--- api/game/index.js
+++ api/game/index.js
@@ -5,16 +5,15 @@ var gameImports = require(path.join(__dirname + '/gameGame'));
 module.exports = function (router) {
 
     router.get('/game', function (req, res) {
-        var butt = 'how bout da';
         console.log('game index get called');
-        gameImports.getSettings((err, save) => {
-            console.log('save loaded');
-            if(err)
+        gameImports.getSettings((err, save) => { // get the setting, the execute the next function(with the args err,save)
+            console.log('game saves loaded');
+            if(err) // if err is defined/!null
             {
-                res.status(500).send(err);
-                return;
+                res.status(500).send(err); //if something went real bad, send server error instead of defining the error
+                return; // exits the function so the next bit of the function isn't run(no point sending bad data)
             }
-            res.send(save);
+            res.send(save); //end point of the function sending the 'data' to the client environment 
         });
     });
 
